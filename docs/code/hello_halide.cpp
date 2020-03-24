6642b24d52190172a74c4fb975718ad141da3fcf
Tzu-Mao Li <tzumao@mit.edu>
2020-03-24 15:59:39 -0400

    comments

--- docs/code/hello_halide.cpp
+++ docs/code/hello_halide.cpp
@@ -7,14 +7,21 @@ using namespace Halide;
 using namespace Halide::Tools;
 
 int main() {
+	// Constructing Halide functions statically.
+    
     ImageParam input(Float(32), 3);
     Func f("f");
     Var x("x"), y("y"), c("c");
     f(x, y, c) = min(2 * input(x, y, c), 1.f);
 
-    // Setup the input by loading an image
+    
+    // Actually compiling/executing the Halide functions.
+    
+    // Setup the input by loading an image.
     Buffer<float> input_buffer = load_and_convert_image("images/rgb.png");
     input.set(input_buffer);
+    // Process the input by calling f.realize
     Buffer<float> output_buffer = f.realize(640, 480, 3);
+    // Save the image to a file.
     convert_and_save_image(output_buffer, "output.png");
 }
